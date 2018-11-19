6b84071d9174d6474746d083b9ae62fbb8fcd1d5
dannyoo7 <dannylanssink@outloo.com>
2018-11-19 14:47:16 +0100

    sql

---
+++ S1-DB-M01-SQL/SQLQuery2.sql
@@ -0,0 +1,3 @@
+create table studenten (id int primary key , roepnaam text , achternaan text);
+insert into studenten(id,roepnaam,achternaan) values (1,'danny','lanssink');
+select * from studenten;

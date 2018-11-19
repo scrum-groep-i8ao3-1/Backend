b430c653daf7e4956b9906dc4571e34ca7b9763f
dannyoo7 <dannylanssink@outloo.com>
2018-11-19 09:48:04 +0100

    2.1 update

--- S1-BE-M02/2.1/2.1/Program.cs
+++ S1-BE-M02/2.1/2.1/Program.cs
@@ -10,6 +10,16 @@ namespace _2._1
     {
         static void Main(string[] args)
         {
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("danny");
+            Console.WriteLine("vandiemenstraat 52");
+            Console.WriteLine("7535an");
+            Console.WriteLine("16");
+            Console.WriteLine("***********************");
+            Console.WriteLine("***danny lanssink***");
+            Console.WriteLine("***********************");
+            Console.ReadKey();
         }
     }
 }
